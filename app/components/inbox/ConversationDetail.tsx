01556428e22105df80722e88cd779ecc52c0c383
Matheus Silva <matheus.souza28042001@gmail.com>
2024-05-13 23:55:03 -0300

    feat: add inbox and conversation page

---
+++ app/components/inbox/ConversationDetail.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import CustomButton from "../forms/CustomButtom";
+
+const ConversationDetail = () => {
+  return (
+    <>
+      <div className="max-h-[400px] overflow-auto flex flex-col space-y-4">
+        <div className="w-4/5 ml-[20%] py-4 px-6 rounded-xl bg-blue-200">
+          <p className="font-bold text-gray-500">Matheus</p>
+          <p>Hello, I'm interested in your property.</p>
+        </div>
+
+        <div className="w-[80%] py-4 px-6 rounded-xl bg-gray-200">
+          <p className="font-bold text-gray-500">John Doe</p>
+          <p>Hello.</p>
+        </div>
+      </div>
+    
+      <div className="mt-4 py-4 px-6 flex border border-gray-300 space-x-4 rounded-xl">
+        <input type="text" className="w-full p-2 bg-gray-200 rounded-xl" placeholder="Type your message..." />
+
+        <CustomButton 
+          label={"Send"} 
+          onClick={() => console.log("clicked")}
+          className={"w-[100px]"}  
+        />
+      </div>
+    </>
+  );
+}
+
+export default ConversationDetail;
